10c913dac6a7267cd619c5d68a34bef2e6b226fa
nithishmaile <132052881+nithishmaile@users.noreply.github.com>
2026-01-08 18:28:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const num1=document.getElementById("n1")
+const num2=document.getElementById("n2")
+const button=document.getElementById("sum_btn")
+const para=document.getElementById("sum")
+
+function calculateSum(){
+	para.textContent=`${parseInt(num1.value)+parseInt(num2.value)}`;
+}
+
+button.addEventListener("click",calculateSum)
